7dd1cdfeb6e0fcfed5e27f1457956f6bfa8c8470
yumiyacchi <gabrielnr93@gmail.com>
2025-08-01 18:12:09 -0400

    añadidos FormatoDinero.kt y FormatoFecha.kt en utilidades

--- app/src/main/java/com/example/deflatam_pruebafinal/utilidades/FormatoDinero.kt
+++ app/src/main/java/com/example/deflatam_pruebafinal/utilidades/FormatoDinero.kt
@@ -1,2 +1,20 @@
 package com.example.deflatam_pruebafinal.utilidades
 
+import java.text.NumberFormat
+import java.util.Locale
+
+object FormatoDinero {
+
+    fun enteroAStringDineroChileno(monto: Int): String {
+        // Locale para Chile (español, Chile)
+        val localeChileno = Locale("es", "CL")
+        val formatoMoneda = NumberFormat.getCurrencyInstance(localeChileno)
+        
+        // El NumberFormat para CLP por defecto no muestra fracciones de moneda.
+        // Si en algún caso se necesitara forzarlo para este u otro Locale:
+        // formatoMoneda.maximumFractionDigits = 0
+        // formatoMoneda.minimumFractionDigits = 0
+
+        return formatoMoneda.format(monto)
+    }
+}
--- app/src/main/java/com/example/deflatam_pruebafinal/utilidades/FormatoFecha.kt
+++ app/src/main/java/com/example/deflatam_pruebafinal/utilidades/FormatoFecha.kt
@@ -6,6 +6,8 @@ import java.util.Locale
 
 object FormatoFecha {
 
+
+    //Funcion para convertir un timestamp a una cadena de fecha en un formato específico.
     fun timestampAFechaString(timestamp: Long, patron: String = "dd/MM/yyyy"): String {
         val sdf = SimpleDateFormat(patron, Locale.getDefault())
         val fecha = Date(timestamp)
