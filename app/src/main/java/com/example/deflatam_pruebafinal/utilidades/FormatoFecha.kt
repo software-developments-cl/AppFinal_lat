1dceb4ad47ade70772d4ef884655ef882e1096ef
yumiyacchi <gabrielnr93@gmail.com>
2025-08-01 17:19:25 -0400

    acualizado FormatoFecha.kt

--- app/src/main/java/com/example/deflatam_pruebafinal/utilidades/FormatoFecha.kt
+++ app/src/main/java/com/example/deflatam_pruebafinal/utilidades/FormatoFecha.kt
@@ -1,2 +1,15 @@
 package com.example.deflatam_pruebafinal.utilidades
 
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
+object FormatoFecha {
+
+    fun timestampAFechaString(timestamp: Long, patron: String = "dd/MM/yyyy"): String {
+        val sdf = SimpleDateFormat(patron, Locale.getDefault())
+        val fecha = Date(timestamp)
+        return sdf.format(fecha)
+    }
+
+}
