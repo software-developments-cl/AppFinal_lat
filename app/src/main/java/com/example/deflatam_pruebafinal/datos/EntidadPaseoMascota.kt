bac90682d9438c1c16b4e6dfa0feb4ffb17db2c6
Anbs12 <118308826+Anbs12@users.noreply.github.com>
2025-08-01 00:06:43 -0400

    Refactor: Update comment in EntidadPaseoMascota.kt
    This commit modifies a comment in the `EntidadPaseoMascota.kt` file.
    The comment was changed from "//Entidad mientras construyen este archivo." to "//Entidad mientras construyen este archivo. para que no me de error".

--- app/src/main/java/com/example/deflatam_pruebafinal/datos/EntidadPaseoMascota.kt
+++ app/src/main/java/com/example/deflatam_pruebafinal/datos/EntidadPaseoMascota.kt
@@ -3,7 +3,7 @@ package com.example.deflatam_pruebafinal.datos
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-//Entidad mientras construyen este archivo.
+//Entidad mientras construyen este archivo. para que no me de error
 
 
 @Entity(tableName = "paseos_mascotas")
